b8664efec29ffd57651f0fdb9ef1c6806a49c7a6
geng <snggeng@gmail.com>
2018-04-28 14:30:23 -0400

    update permission for creating bottle

--- models/user.js
+++ models/user.js
@@ -16,8 +16,8 @@ const UserSchema = new Schema({
   },
   role: {
     type: String,
-    default: 'admin',
-    enum: ['admin', 'manager', 'user'],
+    default: 'user',
+    enum: ['admin', 'user'],
     required: true
   },
   netId: {
--- routes/api.js
+++ routes/api.js
@@ -16,13 +16,13 @@ const bottleController = require('../controllers/bottles')
 router.get('/users/:id', permit('admin', 'user'), userController.getUser)
 router.put('/users/:id', permit('admin', 'user'), userController.updateUser)
 router.delete('/users/:id', permit('admin', 'user'), userController.deleteUser)
-router.get('/users/page/:page/limit/:limit/q', permit('admin', 'user', 'viewer'), userController.getPaginated)
+router.get('/users/page/:page/limit/:limit/q', permit('admin', 'user'), userController.getPaginated)
 
 // BOTTLE ROUTES
 router.get('/bottles/:id', permit('admin', 'user'), bottleController.getBottle)
 router.get('/bottles', permit('admin', 'user'), bottleController.getAll)
-router.post('/bottles', permit('user'), bottleController.createBottle)
-router.put('/bottles/:id', permit('admin'), bottleController.updateBottle)
+router.post('/bottles', permit('admin', 'user'), bottleController.createBottle)
+router.put('/bottles/:id', permit('admin', 'user'), bottleController.updateBottle)
 router.delete('/bottles/:id', permit('admin'), bottleController.deleteBottle)
 router.get('/bottles/:user_id', permit('admin', 'user'), bottleController.getBottlesByUser)
 
